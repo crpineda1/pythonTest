9d6a07d0d1008e865e1655bd01819565fd16776a
crpineda1 <carlos.r.pineda@gmail.com>
2020-04-30 16:18:38 -0400

    lists & sets

--- blog1.py
+++ blog1.py
@@ -1,32 +1,94 @@
 # data types
 
 # String
-str sentence = 'Hi i am a string'
+sentence = 'Hi i am a string'
 
 # Numeric
 
-int number = 1
-float floatt = 2
-complex complexx = 1j
+numberrr = 1 # type int
+floatt = 2.1 # type float
+complexx = 1j # type complex
 
 # Sequence (list = JS array, tuple = JS set?)
-list array = [1,2,3]
-tuple tuplee = (1,2,3) #not sure why this would be useful over a list 
+array = [1,2,3] #type list
+tuplee = (1,2,3) #type set (unique values only) 
+
+array[:2] # [1,2] slice # elements (NOT INDEX)
 
 # Mapping (dictionary = JS array)
-dict dictionary = {name: "John", age: 30}
+dictionary = {"name": "John", "age": 30} #type dict
 
 
 # setup dict from array with keys for dict
 # notice dict.fromkeys() is not camelcase 🤔  
 
 
-list unique_words = ['element1','element2','element3']
+unique_words = ['element1','element2','element3']
 word_histogram = dict.fromkeys(unique_words,0)
 
+list_of_lyrics = ['element1','element2','element3','element1','element2','element1']
 
 # for loop (for...in loop)
 # hash map - increase word count in dict per occurence  
 for word in list_of_lyrics:
   word_histogram[word] = word_histogram[word] + 1
+
+# find the type of of a data type
+type('string') # str
+
+# strings
+name = "homer simpson" # no need for str??
+name.upper() #  'HOME SIMPSON'
+'smithers'.capitalize() # 'Smithers'
+'hello world'.title() # 'Hello World'
+
+name.endswith('simpson') # true
+'charles burns'.endswith('simpson') # false
+
+name.replace('o','0') # 'H0mer Simps0n'
+
+name[:3] # 'geo' -> slice # of characters (same with list)
+
+help(str) #more info on strings and methods (in terminal while running python engine)
+
+# numbers
+
+type(10) # int
+type(10.2) #float
+
+# booleans
+print bool(100) # True
+print bool(0)   # False
+
+# assign empty variable
+address = None
+
+# lists
+
+countries = ['Croatia',
+ 'USA',
+ 'Argentina',
+ 'Mexico',
+ 'USA',
+ 'Morocco',
+ 'USA',
+ 'Finland',
+ 'Argentina',
+ 'Italy',
+ 'Canada',
+ 'South Korea', 
+ 'Malta', 
+ 'Thailand'
+]
+
+countries.pop() # 'Thailand' <-  JS pop
+
+countries.index('USA') # 1j <- JS indexOf
+
+unique_countries = set(countries) # creates set w/unique elemeents
+
+len(countries) # 13 <- length function to count elements
+len(unique_countries) # 10 <- length function to count elements
+
+
 
